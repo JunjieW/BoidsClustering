86a01d79af1bb50bfe7ce65174d43b4c6bfb217a
JunjieW <junjie.wei@dreamcorps.org>
2015-11-09 01:11:27 +0800

    init

---
+++ src/nyu/predictiveanalytics/Core.java
@@ -0,0 +1,13 @@
+package nyu.predictiveanalytics;
+
+import nyu.predictiveanalytics.MyKMeans;
+
+
+public class Core {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/nyu/predictiveanalytics/MyKMeans.java
@@ -0,0 +1,23 @@
+package nyu.predictiveanalytics;
+
+import java.util.Random;
+
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.distance.DistanceMeasure;
+
+public class MyKMeans {
+	private int m_numClusters = -1;
+    private int m_numIterations = -1;
+    private Random m_rg;
+    private DistanceMeasure m_dm;
+    private Instance[] m_centroids;
+    
+	public MyKMeans(){
+		
+	}
+	
+	public void doClustering(){
+		
+	}
+
+}
